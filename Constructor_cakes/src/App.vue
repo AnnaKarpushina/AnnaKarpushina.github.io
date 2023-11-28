9a42e1a2dd30c72132def72b1e2fc29e442eb99f
AnnaKarpushina <47734715+AnnaKarpushina@users.noreply.github.com>
2023-11-29 01:33:09 +0300

    Add files via upload

---
+++ Constructor_cakes/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+	import Cakes from '@/components/Сakes.vue'
+	import Form from '@/components/Form.vue'
+	export default {
+		components: { Cakes, Form }
+	}
+</script>
+
+<template>
+    <div>
+		<Cakes />
+		<Form />
+	</div>
+</template>
