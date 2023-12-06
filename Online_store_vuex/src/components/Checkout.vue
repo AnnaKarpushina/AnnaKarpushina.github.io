8055020720d3bda2778d81891978e6ec25631dcb
AnnaKarpushina <47734715+AnnaKarpushina@users.noreply.github.com>
2023-12-06 17:24:03 +0300

    Delete Online_store_vuex directory

--- Online_store_vuex/src/components/Checkout.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div>
-		<h1 class="mb-3">Заказать сейчас</h1>
-		<div class="alert alert-success">
-			Страница находится в разработке
-		</div>
-	</div>
-</template>
--- Online_store_vuex/src/components/E404.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<div>
-		<h1>Страница не найдена</h1><hr>
-		<router-link :to="{ name: 'catalog' }" class="btn btn-success">Начать с главной страницы</router-link>
-	</div>
-</template>
