807b9f8e428accf6f7396b6ccfd11c429608e1b6
AnnaKarpushina <47734715+AnnaKarpushina@users.noreply.github.com>
2024-02-07 00:49:56 +0300

    Delete Clone_уoutube directory

--- Clone_уoutube/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'json', 'vue'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.vue$': '@vue/vue3-jest'
-  }
-}
