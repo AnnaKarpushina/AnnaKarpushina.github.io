fa21bb86c2c09e5cde011f15da3d2f6779a0edf3
AnnaKarpushina <47734715+AnnaKarpushina@users.noreply.github.com>
2024-02-26 01:42:42 +0300

    Delete OnTrack-composition directory

--- OnTrack-composition/src/components/TheProgressEmptyState.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex grow flex-col items-center gap-4 p-8 text-center">
-    <img src="../assets/img/no_tracked_activities.svg" alt="No tracked activities" class="h-48" />
-    <span class="text-gray-400">У вас нет отслеживаемых действий</span>
-  </div>
-</template>
