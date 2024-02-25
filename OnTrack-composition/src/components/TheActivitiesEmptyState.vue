a46e3164c3443541c005451847032fedfb34460e
AnnaKarpushina <47734715+AnnaKarpushina@users.noreply.github.com>
2024-02-26 01:16:43 +0300

    Add files via upload

---
+++ OnTrack-composition/src/components/TheActivitiesEmptyState.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex grow flex-col items-center gap-4 p-8 text-center">
+    <img src="../assets/img/no_activities.svg" alt="No activities" class="h-48" />
+    <span class="text-gray-400">У вас нет никакой деятельности</span>
+  </div>
+</template>
