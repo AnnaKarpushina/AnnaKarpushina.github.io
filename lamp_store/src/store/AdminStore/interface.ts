e177ea0f46be557d9873e1ee2cf2c8a7f47a98f6
AnnaKarpushina <47734715+AnnaKarpushina@users.noreply.github.com>
2024-11-08 12:34:55 +0300

    Delete lamp_store directory

--- lamp_store/src/store/AdminStore/interface.ts
+++
@@ -1,9 +0,0 @@
-import { ILamp } from '../LampsStore/interface';
-
-export interface IAdminStore {
-    lamps: ILamp[];
-    getAllLamps: () => void;
-    createProduct: (productData: ILamp) => void;
-    updateProduct: (productData: ILamp) => void;
-    deleteProduct: (id: string) => void;
-}
